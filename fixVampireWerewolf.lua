0015f231ed94089efb4b43942f0322509269c3e3
Rolf-TES <107631389+Rolf-TES@users.noreply.github.com>
2024-06-26 00:17:05 +0200

    fixVampireWerewolf.lua

--- fixVampireWerewolf.lua
+++ fixVampireWerewolf.lua
@@ -5,7 +5,7 @@ Description: Fixes the impossibility of becoming a vampire/werewolf without comm
 
 Steps:
 1. Place this file inside 'server\scripts\custom' folder, located in your TES3MP directory.
-2. Open 'customScripts.lua' file ('server\scripts') and write in it the next line: require('custom/finallyVampireWerewolf')
+2. Open 'customScripts.lua' file ('server\scripts') and write in it the next line: require('custom/fixVampireWerewolf')
 3. Save the changes and close it.
 --]]
 
