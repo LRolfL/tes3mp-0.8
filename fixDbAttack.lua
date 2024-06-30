72ce7b2b833a5b2f81a05a3b3172d5c1918847ca
Rolf-TES <107631389+Rolf-TES@users.noreply.github.com>
2024-07-01 00:56:56 +0200

    Add files via upload

---
+++ fixDbAttack.lua
@@ -0,0 +1,39 @@
+--[[
+Written by 'Rolf' for TES3MP 0.8.0/0.8.1. Inspired by Learwolf's dbFix.
+
+Description: .
+
+Steps:
+1. Place this file inside 'server\scripts\custom' folder, located in your TES3MP directory.
+2. Open 'customScripts.lua' file ('server\scripts') and write in it the next line: require('custom/fixDbAttack')
+3. Save the changes and close it.
+--]]
+
+local lvl = 25 -- Level required to activate assassin spawns.
+local spawnOnce = false -- If 'true', the assassin appears only once per player.
+local scriptStatus = {}
+local assassinStatus = {}
+
+local function toggleScript(pid, start)
+    if scriptStatus[pid] ~= start then
+        logicHandler.RunConsoleCommandOnPlayer(pid, (start and 'startscript' or 'stopscript')..' dbAttackScript')
+        scriptStatus[pid] = start
+    end
+end
+
+local function checkLevel(pid)
+    if not assassinStatus[pid] and spawnOnce then
+        toggleScript(pid, tes3mp.GetLevel(pid) >= lvl)
+        assassinStatus[pid] = true
+    elseif not spawnOnce then
+        toggleScript(pid, tes3mp.GetLevel(pid) >= lvl)
+    end
+end
+
+customEventHooks.registerHandler('OnPlayerLevel', function(_, pid) checkLevel(pid) end)
+
+customEventHooks.registerHandler('OnPlayerAuthentified', function(_, pid)
+    if Players[pid] and Players[pid]:IsLoggedIn() then
+        checkLevel(pid)
+    end
+end)
