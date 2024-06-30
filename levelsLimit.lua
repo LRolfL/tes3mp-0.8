edc612b179862e42750a3851cdc9d34289982bfb
Rolf-TES <107631389+Rolf-TES@users.noreply.github.com>
2024-07-01 00:46:36 +0200

    levelsLimit.lua

--- levelsLimit.lua
+++ levelsLimit.lua
@@ -1,5 +1,5 @@
 --[[
-Writen by 'Rolf' for TES3MP 0.8.0 and 0.8.1.
+Written by 'Rolf' for TES3MP 0.8.0/0.8.1.
 
 Description: Set and enforce a level cap for character's level, stats, attributes and skills (TES3MP does not provide individual limit options).
 
