fee693bebc0e300afb0b648ef8cddad3468324ee
Rolf-TES <107631389+Rolf-TES@users.noreply.github.com>
2024-07-01 02:46:53 +0200

    fixVampireWerewolf.lua

--- fixVampireWerewolf.lua
+++ fixVampireWerewolf.lua
@@ -14,28 +14,23 @@ local werewolf = true -- 'false' disables werewolf fix.
 local vampTalk = false -- 'true' allows vampires to talk to everyone, but disables sun damage.
 local tableHelper = require('tableHelper')
 
-local function OnPlayerCellChange(eventStatus, pid)
-    local player = Players[pid]
-    if player and player:IsLoggedIn() then
-        if (tableHelper.containsValue(player.data.spellbook, 'vampire blood quarra') or tableHelper.containsValue(player.data.spellbook, 'vampire blood aundae') or tableHelper.containsValue(player.data.spellbook, 'vampire blood berne')) and vampire and not tableHelper.containsValue(player.data.spellbook, 'vampire attributes') then
-            local vampireSpells = {'set PCVampire to 1', 'addspell "vampire attributes"', 'addspell "vampire skills"', 'addspell "vampire immunities"'}
-            for _, command in pairs(vampireSpells) do
-                logicHandler.RunConsoleCommandOnPlayer(player.pid, command)
+local function OnPlayerCellChange(_, pid)
+    local p = Players[pid]
+    if p and p:IsLoggedIn() then
+        if (tableHelper.containsValue(p.data.spellbook, 'vampire blood quarra') or tableHelper.containsValue(p.data.spellbook, 'vampire blood aundae') or tableHelper.containsValue(p.data.spellbook, 'vampire blood berne')) and vampire and not tableHelper.containsValue(p.data.spellbook, 'vampire attributes') then
+            local vampSpells = {'set PCVampire to 1', 'addspell "vampire attributes"', 'addspell "vampire skills"', 'addspell "vampire immunities"'}
+            for _, comm in pairs(vampSpells) do logicHandler.RunConsoleCommandOnPlayer(p.pid, comm) end
+            if not vampTalk then logicHandler.RunConsoleCommandOnPlayer(p.pid, 'addspell "vampire sun damage"') end
+            if tableHelper.containsValue(p.data.spellbook, 'vampire blood quarra') then
+                logicHandler.RunConsoleCommandOnPlayer(p.pid, 'addspell "vampire quarra specials"')
+            elseif tableHelper.containsValue(p.data.spellbook, 'vampire blood aundae') then
+                logicHandler.RunConsoleCommandOnPlayer(p.pid, 'addspell "vampire aundae specials"')
+            elseif tableHelper.containsValue(p.data.spellbook, 'vampire blood berne') then
+                logicHandler.RunConsoleCommandOnPlayer(p.pid, 'addspell "vampire berne specials"')
             end
-            if vampTalk == false then
-                logicHandler.RunConsoleCommandOnPlayer(player.pid, 'addspell "vampire sun damage"')
-            elseif tableHelper.containsValue(player.data.spellbook, 'vampire blood quarra') then
-                logicHandler.RunConsoleCommandOnPlayer(player.pid, 'addspell "vampire quarra specials"')
-            elseif tableHelper.containsValue(player.data.spellbook, 'vampire blood aundae') then
-                logicHandler.RunConsoleCommandOnPlayer(player.pid, 'addspell "vampire aundae specials"')
-            elseif tableHelper.containsValue(player.data.spellbook, 'vampire blood berne') then
-                logicHandler.RunConsoleCommandOnPlayer(player.pid, 'addspell "vampire berne specials"')
-            end
-        elseif tableHelper.containsValue(player.data.spellbook, 'werewolf blood') and werewolf and not tableHelper.containsValue(player.data.spellbook, 'werewolf resists') then
-            local werewolfSpells = {'set PCWerewolf to 1', 'addspell "werewolf resists"', 'addspell "werewolf regeneration"', 'addspell "werewolf vision"'}
-            for _, command in pairs(werewolfSpells) do
-                logicHandler.RunConsoleCommandOnPlayer(player.pid, command)
-            end
+        elseif tableHelper.containsValue(p.data.spellbook, 'werewolf blood') and werewolf and not tableHelper.containsValue(p.data.spellbook, 'werewolf resists') then
+            local wereSpells = {'set PCWerewolf to 1', 'addspell "werewolf resists"', 'addspell "werewolf regeneration"', 'addspell "werewolf vision"'}
+            for _, comm in pairs(wereSpells) do logicHandler.RunConsoleCommandOnPlayer(p.pid, comm) end
         end
     end
 end
